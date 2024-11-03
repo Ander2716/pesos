ce129409ad763a45842c22ac852e4d26a1796c94
Ander2716 <67704865+Ander2716@users.noreply.github.com>
2024-11-03 18:15:56 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, send_from_directory
+import sys
+from datetime import date
+import json
+
+app = Flask(__name__)
+
+pesos = [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    return render_template("pesos.html")
+
+@app.route("/datos/<float:id>")
+def datos(id):
+    
+    pesos.pop(0)
+    pesos.append(round(id, 2))
+    print(pesos)
+    return ' '
+
+@app.route("/data")
+def data():
+    print(pesos)
+    return json.dumps(pesos)
+
